b23325832cfd2b8438f67f89e50c901e5d4ce9e8
Linwei Chen <linweic@users.noreply.github.com>
2016-03-11 12:38:14 -0500

    binary tree inorder traverse without recursion

--- leetcode/94_BinaryTreeInorderTraversal.java
+++ leetcode/94_BinaryTreeInorderTraversal.java
@@ -23,4 +23,21 @@ public class Solution {
         inorderHelper(list, root.right);
         return;
     }
+    /*iterative method*/
+    public List<Integer> inorderTraversal(TreeNode root){
+        List<Integer> list = new ArrayList<Integer>();
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        if(root == null) return list;
+        TreeNode cur = root;
+        while(cur != null || !stack.isEmpty()){
+            while(cur!=null){
+                stack.push(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            list.add(cur.val);
+            cur = cur.right;
+        }
+        return list;
+    }
 }
