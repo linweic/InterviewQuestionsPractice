9bb645f615320373f81d04b36e748e09f6a06db9
Linwei Chen <linweic@users.noreply.github.com>
2016-03-18 17:54:20 -0400

    utopian tree

--- InterviewQuestion/src/com/linwei/twitter/oa/UtopianTree.java
+++ InterviewQuestion/src/com/linwei/twitter/oa/UtopianTree.java
@@ -1,10 +1,28 @@
 package com.linwei.twitter.oa;
 
 public class UtopianTree {
-
+	public static int getHeight(int n){
+        //if(n == 0) return 1;
+        int[] dp = new int[n+1];
+        //dp[i] denotes the height of tree after i cycles
+        dp[0] = 1;
+        for(int i = 1; i<n+1; i++){
+            if(i % 2 == 1){
+                //spring
+                dp[i] = 2 * dp[i-1];
+            }
+            else{
+                //summer
+                dp[i] = dp[i-1] + 1;
+            }
+        }
+        return dp[n];
+    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println(getHeight(0));
+		System.out.println(getHeight(1));
+		System.out.println(getHeight(4));
 	}
 
 }
