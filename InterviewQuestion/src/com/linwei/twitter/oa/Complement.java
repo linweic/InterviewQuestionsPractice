9efea1d121a9ddb79a4a6f28ca100ae2c59dfa57
Linwei Chen <linweic@users.noreply.github.com>
2016-03-18 17:32:21 -0400

    1's complement

--- InterviewQuestion/src/com/linwei/twitter/oa/Complement.java
+++ InterviewQuestion/src/com/linwei/twitter/oa/Complement.java
@@ -1,10 +1,24 @@
 package com.linwei.twitter.oa;
 
 public class Complement {
-
+	public static int get1sComplement(int n){
+		int m = 1;
+		//int bits = 0;
+		if(n >= 0){
+			while(m <= n){
+				m <<= 1;
+			}
+			m -= 1;
+			return n^m;
+		}
+		else{
+			m = (-1) * (int) Math.pow(2, 31);
+			return m ^ n;
+		}
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println(get1sComplement(-1));
 	}
 
 }
