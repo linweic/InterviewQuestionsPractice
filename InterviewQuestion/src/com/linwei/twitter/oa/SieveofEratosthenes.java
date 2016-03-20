2a2ef6a9e82895e407d14b895c73f4b0934bb8d1
Linwei Chen <linweic@users.noreply.github.com>
2016-03-19 22:56:10 -0400

    wilson theorem

---
+++ InterviewQuestion/src/com/linwei/twitter/oa/SieveofEratosthenes.java
@@ -0,0 +1,56 @@
+package com.linwei.twitter.oa;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * An algorithm to find all the primes less than a given number.
+ * Variant: Wilson's Theorem
+ * @author linweic
+ *
+ */
+public class SieveofEratosthenes {
+	public static List<Integer> getPrimes(int n){
+        boolean isPrime[] = new boolean[n+1];
+        Arrays.fill(isPrime, true);
+        List<Integer> list = new ArrayList<Integer>();
+        isPrime[0] = false;
+        isPrime[1] = false;
+        int divisor = 2;
+        while(divisor <= Math.sqrt(n)){
+            for(int i = divisor+1; i<n+1; i++){
+                if(isPrime[i] == false){
+                    //if i is not prime already, pass it
+                    continue;
+                }
+                else{
+                    //check if i can be divided by divisor, if yes, cross it off
+                    if((i % divisor) == 0) isPrime[i] = false;
+                }
+            }
+            //update divisor as the next number which is not prime
+            for(int i = divisor+1; i<n+1; i++){
+                if(isPrime[i] == true) {
+                    divisor = i;
+                    break;
+                }
+            }
+        }
+        for(int i = 2; i<n+1; i++){
+            if(isPrime[i] == true) list.add(i);
+        }
+        return list;
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		int num = scanner.nextInt();
+		for(int i = 0; i< num; i++){
+		    System.out.println(getPrimes(scanner.nextInt()));
+		}
+		scanner.close();
+	}
+
+}
