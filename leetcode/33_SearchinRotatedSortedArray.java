7368f4471597f344c43259146d0a12a511635dfb
Linwei Chen <linweic@users.noreply.github.com>
2016-04-04 01:02:59 -0400

    33 another solution

--- leetcode/33_SearchinRotatedSortedArray.java
+++ leetcode/33_SearchinRotatedSortedArray.java
@@ -40,4 +40,31 @@ public class Solution {
         }
         return -1;
     }
+    /**
+    *   second solution
+    */
+    public int searchII(int[] nums, int target){
+        if(nums == null || nums.length == 0) return 0;
+        int len = nums.length;
+        int lo = 0;
+        int hi = len-1;
+        while(lo<=hi){
+            int mid = lo + (hi - lo)/2;
+            if(target == nums[mid]) return mid;
+            if(nums[mid] > nums[hi]){
+                //first half is sorted
+                if(nums[lo]<=target && target<nums[mid]) hi = mid - 1;
+                else lo = mid+1;
+            }
+            else if(nums[mid]<nums[hi]){
+                //second half is sorted
+                if(nums[mid]<target && target<=nums[hi]) lo = mid+1;
+                else hi = mid-1;
+            }
+            else{
+                return -1;
+            }
+        }
+        return -1;
+    }
 }
